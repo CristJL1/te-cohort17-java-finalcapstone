8d06e0893b42bc38d0537efa632c54ac5487e0e7
Jared Crist <cristjl1@gmail.com>
2021-08-09 13:33:11 -0400

    Updated Test Data

--- final-capstone/back-end/java/database/Test Data.sql
+++ final-capstone/back-end/java/database/Test Data.sql
@@ -108,39 +108,45 @@ select * from preferences;
 select * from restaurants;
 
 insert into restaurants
-(location_id, restaurant_name, restaurant_phone, restaurant_website, price_range, cuisine_type_1, cuisine_type_2, cuisine_type_3, cuisine_type_4, cuisine_type_5, cuisine_type_6, cuisine_type_7,
+(location_id, restaurant_name, restaurant_phone, restaurant_website, restaurant_description, restaurant_image, price_range, cuisine_type_1, cuisine_type_2, cuisine_type_3, cuisine_type_4, cuisine_type_5, cuisine_type_6, cuisine_type_7,
 address, lat, lon)
-values (12345678, 'Taco Bell', '(330) 652-1234', 'www.tacobell.com', '$', 'Mexican', 'Fast Food', null, null, null, null, null, 
+values (12345678, 'Taco Bell', '(330) 652-1234', 'www.tacobell.com', 'Yo Quiero Taco Bell', 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b3/Taco_Bell_2016.svg/1200px-Taco_Bell_2016.svg.png',
+'$', 'Mexican', 'Fast Food', null, null, null, null, null, 
 '123 Main St, Niles, OH 44446', 40.523687, -2.345678); 
 
 insert into restaurants
-(location_id, restaurant_name, restaurant_phone, restaurant_website, price_range, cuisine_type_1, cuisine_type_2, cuisine_type_3, cuisine_type_4, cuisine_type_5, cuisine_type_6, cuisine_type_7,
+(location_id, restaurant_name, restaurant_phone, restaurant_website, restaurant_description, restaurant_image, price_range, cuisine_type_1, cuisine_type_2, cuisine_type_3, cuisine_type_4, cuisine_type_5, cuisine_type_6, cuisine_type_7,
 address, lat, lon)
-values (98765432, 'Wing Stop', '(330) 394-1234', 'www.wingstop.com', '$$', 'Chicken', 'Fast Food', null, null, null, null, null, 
+values (98765432, 'Wing Stop', '(330) 394-1234', 'www.wingstop.com', 'Where Flavor Gets Its Wings', 'https://www.wingstop.com/assets/images/wingstop-logo-green-340.png',
+'$$', 'Chicken', 'Fast Food', null, null, null, null, null, 
 '456 Market St, Warren, OH 44482', 140.523123, -21.345321); 
 
 insert into restaurants
-(location_id, restaurant_name, restaurant_phone, restaurant_website, price_range, cuisine_type_1, cuisine_type_2, cuisine_type_3, cuisine_type_4, cuisine_type_5, cuisine_type_6, cuisine_type_7,
+(location_id, restaurant_name, restaurant_phone, restaurant_website, restaurant_description, restaurant_image, price_range, cuisine_type_1, cuisine_type_2, cuisine_type_3, cuisine_type_4, cuisine_type_5, cuisine_type_6, cuisine_type_7,
 address, lat, lon)
-values (56238974, 'Hot Dog Shoppe', '(330) 555-1234', 'www.originalhotdogshoppe.com', '$', 'American', 'Burgers', null, null, null, null, null, 
+values (56238974, 'Hot Dog Shoppe', '(330) 555-1234', 'www.originalhotdogshoppe.com', '7 Days Without a Hotdog Makes One Weak', 'https://scontent-mia3-2.xx.fbcdn.net/v/t1.18169-9/10403390_719368844767157_7115626781461645112_n.jpg?_nc_cat=109&ccb=1-4&_nc_sid=09cbfe&_nc_ohc=Ov_EJMPat-AAX9noz_R&_nc_ht=scontent-mia3-2.xx&oh=b7da4287f26dc7aee9d467dc994bc5f9&oe=613619A3',
+'$', 'American', 'Burgers', null, null, null, null, null, 
 '1 Hot Dog Shoppe Alley, Warren, OH 44444', 88.123456, 205.987654); 
 
 insert into restaurants
-(location_id, restaurant_name, restaurant_phone, restaurant_website, price_range, cuisine_type_1, cuisine_type_2, cuisine_type_3, cuisine_type_4, cuisine_type_5, cuisine_type_6, cuisine_type_7,
+(location_id, restaurant_name, restaurant_phone, restaurant_website, restaurant_description, restaurant_image, price_range, cuisine_type_1, cuisine_type_2, cuisine_type_3, cuisine_type_4, cuisine_type_5, cuisine_type_6, cuisine_type_7,
 address, lat, lon)
-values (12457836, 'Maccas', '(330) 123-4567', 'www.maccas.nz', '$', 'American', 'Fast Food', 'Burgers', null, null, null, null, 
+values (12457836, 'Maccas', '(330) 123-4567', 'www.maccas.nz', 'Lovin It', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/2339px-McDonald%27s_Golden_Arches.svg.png',
+'$', 'American', 'Fast Food', 'Burgers', null, null, null, null, 
 '22 Ladies Mile, Glenfield, NZ 12345', -230.523687, -182.345678); 
 
 insert into restaurants
-(location_id, restaurant_name, restaurant_phone, restaurant_website, price_range, cuisine_type_1, cuisine_type_2, cuisine_type_3, cuisine_type_4, cuisine_type_5, cuisine_type_6, cuisine_type_7,
+(location_id, restaurant_name, restaurant_phone, restaurant_website, restaurant_description, restaurant_image, price_range, cuisine_type_1, cuisine_type_2, cuisine_type_3, cuisine_type_4, cuisine_type_5, cuisine_type_6, cuisine_type_7,
 address, lat, lon)
-values (85749612, 'Trattoria', '(440) 123-1234', 'www.trattoriacle.com', '$$$', 'Italian', 'Fine Dining', null, null, null, null, null, 
+values (85749612, 'Trattoria', '(440) 123-1234', 'www.trattoriacle.com', 'Best Italian Food in Cleveland', 'https://static.stacker.com/s3fs-public/styles/slide_desktop/s3/20160809-173338-largejpg_0.jpg',
+'$$$', 'Italian', 'Fine Dining', null, null, null, null, null, 
 '1944 Italia Way, Cleveland, OH 98765', 125.523687, -283.345678); 
 
 insert into restaurants
-(location_id, restaurant_name, restaurant_phone, restaurant_website, price_range, cuisine_type_1, cuisine_type_2, cuisine_type_3, cuisine_type_4, cuisine_type_5, cuisine_type_6, cuisine_type_7,
+(location_id, restaurant_name, restaurant_phone, restaurant_website, restaurant_description, restaurant_image, price_range, cuisine_type_1, cuisine_type_2, cuisine_type_3, cuisine_type_4, cuisine_type_5, cuisine_type_6, cuisine_type_7,
 address, lat, lon)
-values (13467958, 'Panera Bread', '(123) 456-7890', 'www.panerabread.com', '$$', 'Soup', 'Sandwiches', 'Wraps', 'Coffee', 'Bakery', null, null, 
+values (13467958, 'Panera Bread', '(123) 456-7890', 'www.panerabread.com', 'Fresh Bread Makes Friends', 'https://upload.wikimedia.org/wikipedia/en/thumb/c/cb/Panera_Bread_logo.svg/1200px-Panera_Bread_logo.svg.png',
+'$$', 'Soup', 'Sandwiches', 'Wraps', 'Coffee', 'Bakery', null, null, 
 '886 Bread Ave, Cleveland, OH 56234', 45.523687, -79.345678); 
 
 select * from restaurants;
